9d0a0ed881335a5100320547daa05be7a5aff107
Ricardo Lopes <ricardolopesroj@gmail.com>
2023-10-23 22:08:46 -0300

    alterando lista de presente

--- src/pages/IistGifts.tsx
+++ src/pages/IistGifts.tsx
@@ -8,7 +8,7 @@ import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function ListGifts() {
-  const [gifts, setGifts] = useState<Gift[] | null>(null);
+  const [gifts, setGifts] = useState<Gift[]>();
   const [selectedGift, setSelectedGift] = useState<Gift | null>(null);
   const [modalOpen, setModalOpen] = useState(false);
   const [modalOpenConfirm, setModalOpenConfirm] = useState(false);
@@ -35,13 +35,6 @@ export default function ListGifts() {
   })
 
   const renderGifts = () => {
-    if (gifts === null) {
-      return (
-        <div className="text-gold-40 p-4 font-secondary">
-          <Skeleton count={8} />
-        </div>
-      );
-    }
     const indexOfLastGifts = currentPage * 8;
     const indexOFFirtsGifts = indexOfLastGifts - 8;
     let alphabeticGift = gifts.sort((a, b) => {
@@ -202,15 +195,13 @@ export default function ListGifts() {
             <div>
               <div className='font-secondary'>{renderGifts()}</div>
             </div>
-            {gifts && gifts.length > 0 && (
-              <div className='text-white-70 p-4 font-secondary pagination '>
-                {Array.from({ length: Math.ceil(gifts.length / 8) }).map((_, index) => (
-                  <span key={index} onClick={() => paginate(index + 1)} className='m-2 p-2 bg-gold-40 numbers'>
-                    {index + 1}
-                  </span>
-                ))}
-              </div>
-            )}
+            <div className='text-white-70 p-4 font-secondary pagination '>
+              {Array.from({ length: Math.ceil(gifts.length / 8) }).map((_, index) => (
+                <span key={index} onClick={() => paginate(index + 1)} className='m-2 p-2 bg-gold-40 numbers'>
+                  {index + 1}
+                </span>
+              ))}
+            </div>
           </div>
           <div className='flex-center grid'>
             <h1 className='text-gold-40'>Paletas de cores dos presentes</h1>
